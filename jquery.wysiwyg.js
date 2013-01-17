73a2adef0c32cbcaf77e8f263a9a3bd042e10cdd
Igor Starikov <idlesign@yandex.ru>
2013-01-17 16:11:23 +0700

    $.browser fallback for jQuery 1.9+

--- jquery.wysiwyg.js
+++ jquery.wysiwyg.js
@@ -2489,6 +2489,37 @@ html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.o
 	});
 	// end Dialog
 
+	// $.browser fallback for jQuery 1.9+.
+	if ($.browser === undefined) {
+		jQuery.browser = function () {
+			var ua_match = function (ua) {
+				ua = ua.toLowerCase();
+				var match = /(chrome)[ \/]([\w.]+)/.exec(ua) ||
+				/(webkit)[ \/]([\w.]+)/.exec(ua) ||
+				/(opera)(?:.*version|)[ \/]([\w.]+)/.exec(ua) ||
+				/(msie) ([\w.]+)/.exec(ua) ||
+				ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(ua) ||
+				[];
+				
+				return { browser:match[ 1 ] || "", version:match[ 2 ] || "0" };
+			},
+			matched = ua_match(navigator.userAgent),
+			browser = {};
+			
+			if (matched.browser) {
+				browser[ matched.browser ] = true;
+				browser.version = matched.version;
+			}
+			
+			if (browser.chrome) {
+				browser.webkit = true;
+			} else if (browser.webkit) {
+				browser.safari = true;
+			}
+			return browser;
+		}
+	}
+
 	$.fn.wysiwyg = function (method) {
 		var args = arguments, plugin;
 
